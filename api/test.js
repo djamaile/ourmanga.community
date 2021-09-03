033e98804c8bff14504da378cedc79e0bc7b9cdb
Djamaile Rahamat <drahamat@bol.com>
2021-09-03 16:03:10 +0200

    fix: change file path

--- api/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  const { readFileSync } = require("fs");
-  const { join } = require("path");
-  const file = readFileSync(join(__dirname, "pages", "ci.yml"), "utf8");
-  console.info(join(__dirname, "pages", "ci.yml"));
-  res.send(file);
-};
