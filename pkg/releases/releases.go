59a40c3504a6a7ed76bb119373e32c8b3270151d
Djamaile Rahamat <drahamat@bol.com>
2021-09-04 23:24:36 +0200

    fix: viz api request

--- pkg/releases/releases.go
+++ pkg/releases/releases.go
@@ -173,9 +173,9 @@ func CollectVizReleases() []Manga {
 		fmt.Println("Visiting", request.URL.String())
 	})
 
-	//pwd, _ := os.Getwd()
-	//s := fmt.Sprintf("api/pages/viz-%d-%d-%d.html", int(year), int(month), int(day))
-	collector.Visit("file:///var/task/api/pages/viz-2021-9-3.html")
+	pwd, _ := os.Getwd()
+	s := fmt.Sprintf("api/pages/viz-%d-%d-%d.html", int(year), int(month), int(day))
+	collector.Visit("file://" + path.Join(pwd, s))
 
 	return allVizReleases
 }
