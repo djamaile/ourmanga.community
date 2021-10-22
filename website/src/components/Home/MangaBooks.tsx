c76b8a047db1c34ca81031414c758f42aec58588
htaskirmaz <htaskirmaz@bol.com>
2021-10-22 14:14:01 +0200

    Removed unused variables. Added new rule in eslintrc @typescript-eslint/explicit-module-boundary-types: off

--- website/src/components/Home/MangaBooks.tsx
+++ website/src/components/Home/MangaBooks.tsx
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Manga, Mangas } from "../../types";
+import { Manga } from "../../types";
 import { HeartIcon } from "./HeartIcon";
 
 interface Props {
